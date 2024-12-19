a88231d7194ce79bea22e20c602366ebfacd3924
Houssem Eddine ELGAIED <houssemelgaied@gmail.com>
2024-12-19 20:51:03 +0100

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,6 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import { generateToken } from '../utils/generateToken.js';
 import transporter from '../config/email.js';
-
 // @desc     Auth user & get token
 // @method   POST
 // @endpoint /api/users/login
@@ -13,19 +12,31 @@ const loginUser = async (req, res, next) => {
     const { email, password } = req.body;
 
     const user = await User.findOne({ email });
+
     if (!user) {
-      res.status(404);
-      throw new Error('Invalid email address. Please check your email and try again.');
+      res.statusCode = 404;
+      throw new Error(
+        'Invalid email address. Please check your email and try again.'
+      );
     }
 
     const match = await bcrypt.compare(password, user.password);
+
     if (!match) {
-      res.status(401);
-      throw new Error('Invalid password. Please check your password and try again.');
+      res.statusCode = 401;
+      throw new Error(
+        'Invalid password. Please check your password and try again.'
+      );
     }
 
-    generateToken(req, res, user._id);
-
+   const token= generateToken(req, res, user._id);
+      // Setting the JWT as an HTTP-only cookie for enhanced security
+  res.cookie('jwt', token, {
+   
+    secure: false,
+    sameSite: 'None',
+    maxAge: req.body.remember ? 365 * 24 * 60 * 60 * 1000 : 24 * 60 * 60 * 1000
+  });
     res.status(200).json({
       message: 'Login successful.',
       userId: user._id,
@@ -47,8 +58,9 @@ const registerUser = async (req, res, next) => {
     const { name, email, password } = req.body;
 
     const userExists = await User.findOne({ email });
+
     if (userExists) {
-      res.status(409);
+      res.statusCode = 409;
       throw new Error('User already exists. Please choose a different email.');
     }
 
@@ -81,8 +93,7 @@ const registerUser = async (req, res, next) => {
 // @endpoint /api/users/logout
 // @access   Private
 const logoutUser = (req, res) => {
-  res.clearCookie('jwt', { httpOnly: true });
-
+  
   res.status(200).json({ message: 'Logout successful' });
 };
 
@@ -93,8 +104,9 @@ const logoutUser = (req, res) => {
 const getUserProfile = async (req, res, next) => {
   try {
     const user = await User.findById(req.user._id);
+
     if (!user) {
-      res.status(404);
+      res.statusCode = 404;
       throw new Error('User not found!');
     }
 
@@ -117,8 +129,9 @@ const getUserProfile = async (req, res, next) => {
 const admins = async (req, res, next) => {
   try {
     const admins = await User.find({ isAdmin: true });
+
     if (!admins || admins.length === 0) {
-      res.status(404);
+      res.statusCode = 404;
       throw new Error('No admins found!');
     }
     res.status(200).json(admins);
@@ -134,8 +147,9 @@ const admins = async (req, res, next) => {
 const getUsers = async (req, res, next) => {
   try {
     const users = await User.find({ isAdmin: false });
+
     if (!users || users.length === 0) {
-      res.status(404);
+      res.statusCode = 404;
       throw new Error('No users found!');
     }
     res.status(200).json(users);
@@ -143,8 +157,7 @@ const getUsers = async (req, res, next) => {
     next(error);
   }
 };
-
-// @desc     Get user by id
+// @desc     Get user
 // @method   GET
 // @endpoint /api/users/:id
 // @access   Private/Admin
@@ -153,12 +166,14 @@ const getUserById = async (req, res, next) => {
     const { id: userId } = req.params;
     const user = await User.findById(userId);
     if (!user) {
-      res.status(404);
+      res.statusCode = 404;
       throw new Error('User not found!');
     }
     res.status(200).json(user);
   } catch (error) {
-    next(error);
+    res.status(500).json({
+      message: 'Internal Server Error'
+    });
   }
 };
 
@@ -170,13 +185,11 @@ const updateUser = async (req, res, next) => {
   try {
     const { name, email, isAdmin } = req.body;
     const { id: userId } = req.params;
-
     const user = await User.findById(userId);
     if (!user) {
-      res.status(404);
+      res.statusCode = 404;
       throw new Error('User not found!');
     }
-
     user.name = name || user.name;
     user.email = email || user.email;
     user.isAdmin = Boolean(isAdmin);
@@ -185,7 +198,9 @@ const updateUser = async (req, res, next) => {
 
     res.status(200).json({ message: 'User updated', updatedUser });
   } catch (error) {
-    next(error);
+    res.status(500).json({
+      message: 'Internal Server Error'
+    });
   }
 };
 
@@ -198,8 +213,9 @@ const updateUserProfile = async (req, res, next) => {
     const { name, email, password } = req.body;
 
     const user = await User.findById(req.user._id);
+
     if (!user) {
-      res.status(404);
+      res.statusCode = 404;
       throw new Error('User not found. Unable to update profile.');
     }
 
@@ -232,15 +248,12 @@ const updateUserProfile = async (req, res, next) => {
 const deleteUser = async (req, res, next) => {
   try {
     const { id: userId } = req.params;
-
     const user = await User.findById(userId);
     if (!user) {
-      res.status(404);
+      res.statusCode = 404;
       throw new Error('User not found!');
     }
-
     await User.deleteOne({ _id: user._id });
-
     res.status(200).json({ message: 'User deleted' });
   } catch (error) {
     next(error);
@@ -257,25 +270,37 @@ const resetPasswordRequest = async (req, res, next) => {
     const user = await User.findOne({ email });
 
     if (!user) {
-      res.status(404);
+      res.statusCode = 404;
       throw new Error('User not found!');
     }
 
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '15m' });
-    const passwordResetLink = `http://localhost:3000/reset-password/${user._id}/${token}`;
 
-    await transporter.sendMail({
-      from: `"Info shop Tunisia" ${process.env.EMAIL_FROM}`,
-      to: user.email,
-      subject: 'Password Reset',
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '15m'
+    });
+    const passwordResetLink = `http://localhost:3000/reset-password/${user._id}/${token}`;
+    console.log(passwordResetLink);
+   const info= await transporter.sendMail({
+      from: `"Info shop Tunisia" ${process.env.EMAIL_FROM}`, // sender address
+      to: user.email, // list of receivers
+      subject: 'Password Reset', // Subject line
+      text: "Hello this is the link to change ur password ", 
       html: `<p>Hi ${user.name},</p>
-             <p>We received a password reset request for your account. Click the link below to set a new password:</p>
-             <p><a href=${passwordResetLink} target="_blank">${passwordResetLink}</a></p>
-             <p>If you didn't request this, you can ignore this email.</p>
-             <p>Thanks,<br>INFIO SHOP TUNISIA</p>`
+
+            <p>We received a password reset request for your account. Click the link below to set a new password:</p>
+
+            <p><a href=${passwordResetLink} target="_blank">${passwordResetLink}</a></p>
+
+            <p>If you didn't request this, you can ignore this email.</p>
+
+            <p>Thanks,<br>
+            INFIO SHOP TUNISIA </p>` // html body
     });
 
-    res.status(200).json({ message: 'Password reset email sent, please check your email.' });
+    res
+      .status(200)
+      .json({ message: 'Password reset email sent, please check your email.' });
   } catch (error) {
     next(error);
   }
@@ -285,11 +310,13 @@ const resetPasswordRequest = async (req, res, next) => {
 // @method   POST
 // @endpoint /api/users/reset-password/reset/:id/:token
 // @access   Private
+
 const resetPassword = async (req, res, next) => {
   try {
-    const { password } = req.body;
-    const { id: userId, token } = req.params;
-
+    const { password } = req.body; // Nouveau mot de passe envoyé dans la requête
+    const { id: userId, token } = req.params; // ID utilisateur et token dans l'URL
+    
+    // Vérification du token JWT
     let decodedToken;
     try {
       decodedToken = jwt.verify(token, process.env.JWT_SECRET);
@@ -297,28 +324,37 @@ const resetPassword = async (req, res, next) => {
       return res.status(401).json({ message: 'Invalid or expired token' });
     }
 
+    // Vérification si le token correspond à l'utilisateur
     if (decodedToken.userId !== userId) {
       return res.status(400).json({ message: 'Token does not match user ID' });
     }
 
+    // Récupérer l'utilisateur dans la base de données
     const user = await User.findById(userId);
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
     }
 
+    // Vérification que le nouveau mot de passe n'est pas identique à l'ancien
     if (bcrypt.compareSync(password, user.password)) {
       return res.status(400).json({ message: 'New password cannot be the same as the old password' });
     }
 
+    // Hacher le nouveau mot de passe avant de le sauvegarder
     const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Sauvegarder le nouveau mot de passe
     user.password = hashedPassword;
     await user.save();
 
+    // Réponse de succès
     res.status(200).json({ message: 'Password successfully reset' });
+
   } catch (error) {
     next(error);
   }
 };
+
 
 export {
   loginUser,
