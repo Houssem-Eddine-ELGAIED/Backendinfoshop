a8893313d29ed2b605c2d96d06bb5b4f060d4ec8
Houssem Eddine ELGAIED <houssemelgaied@gmail.com>
2024-12-19 16:38:22 +0100

    Update generateToken.js

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,19 +1,16 @@
 import jwt from 'jsonwebtoken';
 
 export const generateToken = (req, res, userId) => {
-  try {
-    // Générer le token JWT
-    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-      expiresIn: req.body.remember ? '365d' : '24h' // Durée configurable
-    });
+  // Generating a JWT token for the authenticated user
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: req.body.remember ? 365 * 24 + 'h' : '24h'
+  });
 
-    // Retourner le token dans la réponse JSON
-    res.status(200).json({
-      message: 'Token généré avec succès',
-      token // Retourne le token au client
-    });
-  } catch (error) {
-    console.error('Erreur lors de la génération du token:', error);
-    res.status(500).json({ error: 'Erreur interne lors de la génération du token' });
-  }
+  // Setting the JWT as an HTTP-only cookie for enhanced security
+  res.cookie('jwt', token, {
+    httpOnly: false,
+    secure: process.env.NODE_ENV !== 'development',
+    sameSite: 'strict',
+    maxAge: req.body.remember ? 365 * 24 * 60 * 60 * 1000 : 24 * 60 * 60 * 1000
+  });
 };
